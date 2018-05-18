2795294cbd70dedae6a808eedb04db5cd033b817
ddattee <david.dattee@shopping-flux.com>
2018-05-18 11:06:14 +0200

    #13 - UT - adding ut for order resource and updating store's

--- tests/unit/Api/Store/StoreResourceTest.php
+++ tests/unit/Api/Store/StoreResourceTest.php
@@ -27,7 +27,7 @@ class StoreResourceTest extends Sdk\Test\Api\AbstractResourceTest
         $this->assertTrue($instance->isActive());
     }
 
-    public function testGetInventory()
+    public function testGetInventoryApi()
     {
         /** @var Sdk\Hal\HalResource|\PHPUnit_Framework_MockObject_MockObject $halResource */
         $halResource = $this->createMock(Sdk\Hal\HalResource::class);
@@ -42,5 +42,22 @@ class StoreResourceTest extends Sdk\Test\Api\AbstractResourceTest
         $instance = new Sdk\Api\Store\StoreResource($halResource);
 
         $this->assertInstanceOf(Sdk\Api\Catalog\InventoryDomain::class, $instance->getInventoryApi());
+    }
+
+    public function testGetOrderApi()
+    {
+        /** @var Sdk\Hal\HalResource|\PHPUnit_Framework_MockObject_MockObject $halResource */
+        $halResource = $this->createMock(Sdk\Hal\HalResource::class);
+        $halResource
+            ->expects($this->once())
+            ->method('getLink')
+            ->with('order')
+            ->willReturn(
+                $this->createMock(Sdk\Hal\HalLink::class)
+            );
+
+        $instance = new Sdk\Api\Store\StoreResource($halResource);
+
+        $this->assertInstanceOf(Sdk\Api\Order\OrderDomain::class, $instance->getOrderApi());
     }
 }
